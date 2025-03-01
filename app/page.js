d8d9a1f437f93a9047c77f7df0babeba1db32924
drong24 <daisyrong24@gmail.com>
2025-02-28 23:35:39 -0800

    added title and body to homepage

--- app/page.js
+++ app/page.js
@@ -1,6 +1,11 @@
 
 export default function HomePage() {
     return (
-        <div>+</div>
+        <div class="main">
+            <h1>Cardfolio</h1>
+            <section>About Me</section>
+            <section>Projects</section>
+            <section>Contact Me</section>
+        </div>
     );
 }
